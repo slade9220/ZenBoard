af9ceb7ebb619bac75db225ae69ea69fc2beed70
Gennaro Amura <gennaroamura@10-20-32-17.0100.fed.it.iosda.org>
2018-05-04 16:50:00 +0200

    Initial Commit

---
+++ TouchCanvas/AppDelegate.swift
@@ -0,0 +1,15 @@
+/*
+    Copyright (C) 2018 Gennaro Amura All Rights Reserved.
+    
+    
+    Abstract:
+    The application delegate.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
